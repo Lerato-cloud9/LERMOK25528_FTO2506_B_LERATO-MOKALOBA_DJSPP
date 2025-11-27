c804528b7d4b0ff83f6e550bd5595cd4a04eaea8
Lerato Mokaloba <mokalobalerato@gmail.com>
2025-11-27 12:14:41 +0200

    Added a theme toggle  to the import and by the header section

--- src/components/UI/Header.jsx
+++ src/components/UI/Header.jsx
@@ -1,12 +1,14 @@
-import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
+import ThemeToggle from "../Filters/ThemeToggle";
+import styles from "./Header.module.css";
+
 export default function Header() {
   return (
     <header className={styles.appHeader}>
       <h1>
-        {" "}
         <Link to="/">🎙️ Podcast App</Link>
       </h1>
+      <ThemeToggle />
     </header>
   );
 }
